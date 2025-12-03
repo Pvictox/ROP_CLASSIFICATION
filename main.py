e44f6c893fbb9e564fef236ef373d5bbd6dae9a4
Pedro <paf.victor@gmail.com>
2025-12-03 22:07:41 +0000

    [ped] fazendo coisasa

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from data_factory.ROP_dataset import ROPDataset
 from train_and_val_worker import TrainAndEvalWorker
 import os
 
-import optuna
+
 from optuna_trials import OptunaTrials
-from dotenv import load_dotenv
-
-load_dotenv()
-
-db_url = os.getenv('DB_URL')
 
 
 
-IMG_FILE_PATH = '/home/pedro_fonseca/PATIENT_ROP/DATASET'
-CSV_FILE_PATH = '/home/pedro_fonseca/PATIENT_ROP/DATASET/infant_retinal_database_info.csv'
+
+# db_url = os.getenv('DB_URL')
+
+
+
+IMG_FILE_PATH = '/backup/pedro_fonseca/PATIENT_ROP/DATASET'
+CSV_FILE_PATH = '/backup/pedro_fonseca/PATIENT_ROP/DATASET/infant_retinal_database_info.csv'
 
 # IMG_FILE_PATH = '/backup/lucas/rop_dataset/images_stack_without_captions/images_stack_without_captions'
 # CSV_FILE_PATH = '/backup/lucas/rop_dataset/infant_retinal_database_info.csv'
@@ -37,42 +37,41 @@ def main():
         rop_dataset = ROPDataset(df, is_train=False, apply_clahe=True)
         # X_train, y_train, train_indx, patient_ids_train, gkf, test_dataset = data_factory.prepare_data_for_cross_validation(rop_dataset)
         X_train, y_train, train_indx, patient_ids_train, gkf, test_dataset = data_factory.prepare_data_for_cross_validation_3(rop_dataset, num_splits=3)
+        X_train, y_train, train_indx, patient_ids_train, gkf, test_dataset = data_factory.prepare_data_for_cross_validation_3(rop_dataset, num_splits=3)
 
         train_full_subset, val_full_subset, test_subset = data_factory.prepare_train_val_and_test_datasets(rop_dataset)
 
-        pruner = optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=0, interval_steps=1)
-        study = optuna.create_study(direction='maximize', pruner=pruner, study_name='dynamic_efficientnet_optimization_trials_final_MEU_DEUS', storage=db_url, load_if_exists=True
-                    )
-        optuna_trials = OptunaTrials()
-        N_TRIALS = 50
-        try:
-            study.optimize(lambda trial: optuna_trials.objective(trial, X_train, y_train, patient_ids_train, train_indx, gkf, rop_dataset, num_trials=N_TRIALS, full_train_subset=train_full_subset, full_val_subset=val_full_subset, test_subset=test_subset), n_trials=N_TRIALS)
-        except KeyboardInterrupt:
-            print("Otimização interrompida pelo usuário.")
+        # pruner = optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=0, interval_steps=1)
+        # study = optuna.create_study(direction='maximize', pruner=pruner, study_name='dynamic_efficientnet_optimization_trials_final_MEU_DEUS', storage=db_url, load_if_exists=True
+        #             )
+        # optuna_trials = OptunaTrials()
+        # N_TRIALS = 50
+        # try:
+        #     study.optimize(lambda trial: optuna_trials.objective(trial, X_train, y_train, patient_ids_train, train_indx, gkf, rop_dataset, num_trials=N_TRIALS, full_train_subset=train_full_subset, full_val_subset=val_full_subset, test_subset=test_subset), n_trials=N_TRIALS)
+        # except KeyboardInterrupt:
+        #     print("Otimização interrompida pelo usuário.")
 
-        worker = TrainAndEvalWorker(config=None, model=None)
+        #worker = TrainAndEvalWorker(config=None, model=None)
         # results = worker.evaluate(test_subset, model_path='saved_models/best_dynamic_efficientnet.pth')
         #optuna_trials.save_best_model()
-        print("\n--- Otimização Concluída ---")
-        print(f"Melhor trial: {study.best_trial.number}")
-        print(f"Melhor Acurácia de Validação: {study.best_value:.4f}")
+        # print("\n--- Otimização Concluída ---")
+        # print(f"Melhor trial: {study.best_trial.number}")
+        # print(f"Melhor Acurácia de Validação: {study.best_value:.4f}")
         
-        print("\nMelhor Arquitetura Encontrada:")
-        for key, value in study.best_params.items():
-            print(f"  {key}: {value}")
+        # print("\nMelhor Arquitetura Encontrada:")
+        # for key, value in study.best_params.items():
+        #     print(f"  {key}: {value}")
 
-        #Salvando melhor arquitetura em um arquivo de texto
-        with open('best_architecture.txt', 'w') as f:
-            f.write(f"Melhor trial: {study.best_trial.number}\n")
-            f.write(f"Melhor Acurácia de Validação: {study.best_value:.4f}\n")
-            f.write("\nMelhor Arquitetura Encontrada:\n")
-            for key, value in study.best_params.items():
-                f.write(f"{key}: {value}\n")
-        # train_and_val_worker = TrainAndEvalWorker(config=None)
-        # print("Iniciando o treinamento e validação com GroupKFold...")
-        # train_and_val_worker.train(X_train, y_train, patient_ids_train, train_indx, gkf, rop_dataset)
-        # print("Iniciando a avaliação no conjunto de teste...")
-        # results = train_and_val_worker.evaluate(test_dataset, model_path='saved_models/best_model_efficientNET.pth')
+        # #Salvando melhor arquitetura em um arquivo de texto
+        # with open('best_architecture.txt', 'w') as f:
+        #     f.write(f"Melhor trial: {study.best_trial.number}\n")
+        #     f.write(f"Melhor Acurácia de Validação: {study.best_value:.4f}\n")
+        #     f.write("\nMelhor Arquitetura Encontrada:\n")
+        #     for key, value in study.best_params.items():
+        #         f.write(f"{key}: {value}\n")
+        train_and_val_worker = TrainAndEvalWorker(config=None)
+        print("Iniciando a avaliação no conjunto de teste...")
+        results = train_and_val_worker.evaluate(test_dataset, model_path='/backup/pedro_fonseca/PATIENT_ROP/saved_models/best_dynamic_efficientnet.pth')
 
         #     #Dividir em treino e teste
             #Treino e evalidação por paciente
